c207c0ac5b5f02947fe325471c1c61a4167bb2ff
Федін Володимир <162556359+Class1c22@users.noreply.github.com>
2025-10-11 18:13:45 +0300

    Add files via upload

---
+++ src/core/security.py
@@ -0,0 +1,19 @@
+from passlib.context import CryptContext
+from jose import jwt
+from datetime import datetime, timedelta
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+SECRET_KEY = "super_secret_key"
+ALGORITHM = "HS256"
+
+def hash_password(password: str) -> str:
+    return pwd_context.hash(password)
+
+def verify_password(plain: str, hashed: str) -> bool:
+    return pwd_context.verify(plain, hashed)
+
+def create_token(data: dict):
+    to_encode = data.copy()
+    expire = datetime.utcnow() + timedelta(hours=1)
+    to_encode.update({"exp": expire})
+    return jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
